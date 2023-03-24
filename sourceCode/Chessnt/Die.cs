c5d133eff35f67760c798d9e55d879ecce447534
Tung Xuan Do <doxuantung257@gmail.com>
2023-03-24 13:33:18 +0100

    half baked text outline for dice

--- sourceCode/Chessnt/Die.cs
+++ sourceCode/Chessnt/Die.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Chessnt
 {
@@ -16,9 +18,13 @@ namespace Chessnt
         private int _maxRollCount;
         private int _width = 260;
         private int _height = 300;
+        private TextOutline _textOutline;
+        private SpriteFont _font;
 
-        public Die(Texture2D texture, Vector2 position)
+        public Die(Texture2D texture, Vector2 position, ContentManager content)
         {
+            _font = content.Load<SpriteFont>("Font");
+            _textOutline = new TextOutline(_font);
             _texture = texture;
             _position = position;
             _isRolling = false;
@@ -65,6 +71,7 @@ namespace Chessnt
         {
             Rectangle destRect = new Rectangle((int)_position.X, (int)_position.Y, _width, _height);
             spriteBatch.Draw(_texture, destRect, Color.White);
+            _textOutline.DrawTextOutLine(_value.ToString(), _position.X + _width / 2, _position.Y + _height / 2, 0.2f, spriteBatch);
             spriteBatch.DrawString(font, _value.ToString(), new Vector2(_position.X + _width / 2, _position.Y + _height / 2), Color.White, 0f, new Vector2(font.MeasureString(_value.ToString()).X / 2, font.MeasureString(_value.ToString()).Y / 2), 1f, SpriteEffects.None, 0f);
         }
     }
