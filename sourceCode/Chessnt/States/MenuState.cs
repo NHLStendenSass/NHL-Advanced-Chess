351365b2f6ef81abca71a920676b776da8724200
Tung Xuan Do <doxuantung257@gmail.com>
2023-04-06 14:55:38 +0200

    update

--- sourceCode/Chessnt/States/MenuState.cs
+++ sourceCode/Chessnt/States/MenuState.cs
@@ -69,12 +69,12 @@ namespace Chessnt
             };
             _voiceButton.Click += VoiceButton_Click;
 
-            voiceButton = new Button(buttonTexture, buttonFont)
+            _voiceButton = new Button(_buttonTexture, _buttonFont)
             {
                 Position = new Vector2(1400, 170),
                 Text = "Talk",
             };
-            voiceButton.Click += VoiceButton_Click;
+            _voiceButton.Click += VoiceButton_Click;
 
             _components = new List<Component>()
                   {
@@ -129,11 +129,6 @@ namespace Chessnt
         private void VoiceButton_Click(object sender, EventArgs e)
         {
             _voiceCommand.RecognitionWithMicrophoneAsync().Wait();
-        }
-
-        private void VoiceButton_Click(object sender, EventArgs e)
-        {
-            voiceCommand.RecognitionWithMicrophoneAsync().Wait();
         }
 
         public override void PostUpdate(GameTime gameTime)
