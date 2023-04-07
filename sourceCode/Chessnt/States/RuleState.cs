39c06e35ca4856e5f14351e17ce9dc1c3d8c8a89
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2023-04-07 12:46:15 +0200

    update to rule state

--- sourceCode/Chessnt/States/RuleState.cs
+++ sourceCode/Chessnt/States/RuleState.cs
@@ -6,25 +6,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Chessnt.Utilities;
 
 namespace Chessnt
 {
-    public class OptionState : State
+    public class RuleState : State
     {
         private List<Component> _components;
         private Texture2D _backgroundTexture;
         private Texture2D _buttonTexture;
         private SpriteFont _buttonFont;
         private Button _saveButton;
-        private Utilities.TextOutline _textOutline;
+        private TextOutline _textOutline;
 
-        public OptionState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
+        public RuleState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
           : base(game, graphicsDevice, content)
         {
             _backgroundTexture = base.content.Load<Texture2D>("option_background");
             _buttonTexture = base.content.Load<Texture2D>("Button");
             _buttonFont = base.content.Load<SpriteFont>("Font");
-            _textOutline = new Utilities.TextOutline(_buttonFont);
+            _textOutline = new TextOutline(_buttonFont);
 
             _saveButton = new Button(_buttonTexture, _buttonFont)
             {
@@ -36,6 +37,8 @@ namespace Chessnt
             {
                 _saveButton,
             };
+
+            Text text = new Text("Hello World!", 100, 100, 1.0f, 20, Color.White);
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
@@ -45,18 +48,20 @@ namespace Chessnt
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Begin();
 
             spriteBatch.Draw(_backgroundTexture, new Vector2(0, 0), Color.White);
 
-            DrawOptionTexts("Option", 650, 50, 1.015f, spriteBatch);
-            DrawOptionTexts("Voice:", 150, 250, 1.015f, spriteBatch);
-            DrawOptionTexts("Dice:", 150, 450, 1.015f, spriteBatch);
-            DrawOptionTexts("Bruh:", 150, 650, 1.015f, spriteBatch);
+            DrawOptionTexts("Rules", 650, 50, 1.015f, spriteBatch);
+            //DrawOptionTexts("Voice:", 150, 250, 0.9f, spriteBatch);
+            //DrawOptionTexts("Dice:", 150, 450, 1.015f, spriteBatch);
+            //DrawOptionTexts("Bruh:", 150, 650, 1.015f, spriteBatch);
+            DrawContent("Hello World!", 100, 100, 1.0f, spriteBatch);
+
 
             DrawComponents(gameTime, spriteBatch);
 
-
+            spriteBatch.End();
         }
 
         private void DrawComponents(GameTime gameTime, SpriteBatch spriteBatch)
@@ -78,6 +83,17 @@ namespace Chessnt
             {
                 component.Update(gameTime);
             }
+        }
+
+        public void DrawText(Text text, SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(_buttonFont, text.Content, new Vector2(text.X, text.Y), text.Color, text.Scale, Vector2.Zero, text.FontSize, SpriteEffects.None, 0f);
+        }
+
+        private void DrawContent(string text, int textX, int textY, float textScale, SpriteBatch spriteBatch)
+        {
+            //_textOutline.DrawTextOutLine(text, textX, textY, textScale, spriteBatch);
+            spriteBatch.DrawString(_buttonFont, text, new Vector2(textX, textY), Color.White, 0f, Vector2.Zero, textScale, SpriteEffects.None, 0f);
         }
 
         private void DrawOptionTexts(string text, int textX, int textY, float textScale, SpriteBatch spriteBatch)
---
+++ sourceCode/Chessnt/Utilities/Text.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chessnt.Utilities
+{
+    public class Text
+    {
+        public string Content { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public float Scale { get; set; }
+        public int FontSize { get; set; }
+        public Color Color { get; set; }
+
+        public Text(string content, int x, int y, float scale, int fontSize, Color color)
+        {
+            Content = content;
+            X = x;
+            Y = y;
+            Scale = scale;
+            FontSize = fontSize;
+            Color = color;
+        }
+    }
+}
